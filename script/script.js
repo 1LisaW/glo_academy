38397943344287122dec59c29e44cd38163d444a
LisaW <LisaW@ya.ru>
2019-06-28 08:41:34 +0300

    add script for homework lesson02

--- script/script.js
+++ script/script.js
@@ -1,9 +1,17 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let perion;
+// money- доход за месяц
+let money = 10000;
+let income = "freelance";
+let addExpenses = "Cat1,cat2,cat3";
+let deposit = false;
+let mission = 1000000;
+let perion =12;
+let budgetDay =money/30;
 
-alert( 'Random message' );
-console.log( 'Random text' );
+console.log( typeof(money) );
+console.log( typeof(income) );
+console.log( typeof(deposit) );
+console.log( income.length );
+console.log ( "Период "+perion+" месяцев" );
+console.log( "Цель заработать "+ mission+ " юани" );
+console.log( addExpenses.toLowerCase().split( "," ) );
+console.log( "Дневной доход: "+budgetDay+ ", остаток от деления: "+ money%30 );
