3561397e0f441d53506a63a1376a2b3d2142f9f9
LisaW <LisaW@ya.ru>
2019-07-27 22:51:47 +0300

    change regexp

--- 2019_07_27_lesson17/script/script.js
+++ 2019_07_27_lesson17/script/script.js
@@ -447,7 +447,16 @@ document.addEventListener('click',(event)=>{
         userPhone.forEach( item => {
             item.addEventListener('input', (event) => {
                 const target = event.target;
-                target.value = target.value.replace(/[^\d\+]/gi,'');
+                target.value = target.value.replace(/[^0-9+]/i,'');
+                target.value = target.value.replace(/(.)\+/i,'$1');
+            });
+    
+        });
+        const userMessage = document.querySelectorAll('[name=user_message]');
+        userMessage.forEach(item => {
+            item.addEventListener('input', (event) => {
+                const target = event.target;
+                target.value = target.value.replace(/[^а-яё ]/gi,'');
             });
     
         });
