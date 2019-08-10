fa32409469cd87ac1f16ea216df6be79e4d4ff2d
LisaW <LisaW@ya.ru>
2019-08-10 15:26:00 +0300

    fix behavior of buttons try

--- diplom_insein/src/modules/togglePopupTry.js
+++ diplom_insein/src/modules/togglePopupTry.js
@@ -1,11 +1,14 @@
 const togglePopupTry = () =>{
-    const buttonTryProduct = document.querySelector('.main-try');
+    const buttonTryProduct = document.querySelectorAll('.main-try');
     const popupWindow = document.querySelector('.modal_offer');
     const closeBtn = document.querySelectorAll('[aria-label="Close"]');
-    buttonTryProduct.addEventListener('click', (event) =>{
+    buttonTryProduct.forEach((item)=>{
+        item.addEventListener('click', (event) =>{
+            if (item === buttonTryProduct[8]){
+                return;
+            }
         popupWindow.style.cssText = "display:block;position: fixed;top: 50%;left: 50%;transform: translate(-50%, -50%);width:70%";
-        // popupWindow.style.top = `${(window.innerWidth-popupWindow.offsetWidth)/2}px`;
-        // popupWindow.style.left = `${(window.innerHeight-popupWindow.offsetHeight)/2}px`;
+        });
     });
     closeBtn[1].addEventListener('click', ()=>{
         popupWindow.style.display = 'none';
